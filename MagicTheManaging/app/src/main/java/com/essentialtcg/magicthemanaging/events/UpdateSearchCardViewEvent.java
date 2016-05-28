e24dfd58a81649c98fbf868330e93fcb597637b8
Hankahn <hankahn@gmail.com>
2016-05-28 13:50:16 -0400

    Added new event for notifying change of search parameters

---
+++ MagicTheManaging/app/src/main/java/com/essentialtcg/magicthemanaging/events/UpdateSearchCardViewEvent.java
@@ -0,0 +1,16 @@
+package com.essentialtcg.magicthemanaging.events;
+
+import com.essentialtcg.magicthemanaging.data.CardSearchParameters;
+
+/**
+ * Created by Shawn on 5/28/2016.
+ */
+public class UpdateSearchCardViewEvent {
+
+    public CardSearchParameters searchParameters;
+
+    public UpdateSearchCardViewEvent(CardSearchParameters searchParameters) {
+        this.searchParameters = searchParameters;
+    }
+
+}
