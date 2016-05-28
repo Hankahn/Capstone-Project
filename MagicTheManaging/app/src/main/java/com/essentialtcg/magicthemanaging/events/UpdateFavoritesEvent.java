90c20b6e4fe26861e9267cf087c8fe72be4a6d25
Hankahn <hankahn@gmail.com>
2016-05-28 02:14:59 -0400

    Added UpdateFavorites EventBus event

---
+++ MagicTheManaging/app/src/main/java/com/essentialtcg/magicthemanaging/events/UpdateFavoritesEvent.java
@@ -0,0 +1,12 @@
+package com.essentialtcg.magicthemanaging.events;
+
+/**
+ * Created by Shawn on 5/28/2016.
+ */
+public class UpdateFavoritesEvent {
+
+    public void UpdateFavoritesEvent() {
+
+    }
+
+}
