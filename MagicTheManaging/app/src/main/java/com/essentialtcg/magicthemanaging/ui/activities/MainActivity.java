f31432ae144a771b8c4e9f2f7d62bd2a49da04f6
Hankahn <hankahn@gmail.com>
2016-05-28 02:07:41 -0400

    Lots of changes

--- MagicTheManaging/app/src/main/java/com/essentialtcg/magicthemanaging/ui/activities/MainActivity.java
+++ MagicTheManaging/app/src/main/java/com/essentialtcg/magicthemanaging/ui/activities/MainActivity.java
@@ -3,11 +3,13 @@ package com.essentialtcg.magicthemanaging.ui.activities;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -18,14 +20,20 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
 
 import com.essentialtcg.magicthemanaging.R;
 import com.essentialtcg.magicthemanaging.callback.DrawerAdapterCallback;
-import com.essentialtcg.magicthemanaging.callback.UpdateRecyclerViewCallback;
+import com.essentialtcg.magicthemanaging.callback.LoadCardDetailCallback;
+import com.essentialtcg.magicthemanaging.data.CardSearchParameters;
+import com.essentialtcg.magicthemanaging.data.items.SetItem;
+import com.essentialtcg.magicthemanaging.events.UpdateRecyclerViewPositionReturnEvent;
+import com.essentialtcg.magicthemanaging.ui.fragments.CardViewFragment;
 import com.essentialtcg.magicthemanaging.ui.fragments.FavoritesFragment;
 import com.essentialtcg.magicthemanaging.ui.fragments.SearchFragment;
 import com.essentialtcg.magicthemanaging.adapters.DrawerAdapter;
+import com.essentialtcg.magicthemanaging.utils.SetArrayList;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -36,9 +44,12 @@ import com.google.android.gms.common.api.OptionalPendingResult;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 
+import org.greenrobot.eventbus.EventBus;
+
 public class MainActivity extends AppCompatActivity
         implements DrawerAdapterCallback,
-        GoogleApiClient.OnConnectionFailedListener {
+        GoogleApiClient.OnConnectionFailedListener,
+        LoadCardDetailCallback {
 
     private String TAG = "MainActivity";
 
@@ -58,7 +69,6 @@ public class MainActivity extends AppCompatActivity
     private RecyclerView.Adapter mDrawerAdapter;
     private RecyclerView.LayoutManager mDrawerLayoutManager;
     private Fragment mFragment;
-    private UpdateRecyclerViewCallback mUpdateRecyclerViewCallback;
     private Bundle mReenterState;
     private ActionBarDrawerToggle mDrawerToggle;
     private MenuItem mSignInMenuItem;
@@ -108,28 +118,50 @@ public class MainActivity extends AppCompatActivity
 
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
-        SearchFragment searchFragment;
-
         if (savedInstanceState == null) {
-            searchFragment = new SearchFragment();
+            SearchFragment searchFragment = new SearchFragment();
 
             mFragment = searchFragment;
-
-            mUpdateRecyclerViewCallback = searchFragment;
 
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.fragment_container, mFragment, "SearchFragment")
                     .commit();
+
+            if (getResources().getBoolean(R.bool.multipane)) {
+                // TODO: Pull this from the sharedpreferences instead
+                CardSearchParameters cardSearchParameters = new CardSearchParameters();
+
+                SetArrayList setFilter = new SetArrayList();
+                SetItem setItem = new SetItem();
+                setItem.setName("Oath of the Gatewatch");
+                setItem.setCode("OGW");
+                setFilter.add(setItem);
+
+                cardSearchParameters.setSetFilter(setFilter);
+
+                CardViewFragment cardViewFragment = CardViewFragment.newInstance(0, 0, cardSearchParameters);
+
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container_right, cardViewFragment, "CardViewFragment")
+                        .commit();
+            }
         } else {
-            searchFragment = (SearchFragment) getSupportFragmentManager().findFragmentByTag("SearchFragment");
-            mFragment = searchFragment;
-            mUpdateRecyclerViewCallback = searchFragment;
+            Fragment currentFragment =
+                    getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+
+            if (currentFragment.getClass().equals(SearchFragment.class)) {
+                mFragment = currentFragment;
+            } else if (currentFragment.getClass().equals(FavoritesFragment.class)) {
+                mFragment = currentFragment;
+            }
         }
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+
+        checkLoginStatus();
     }
 
     private void checkLoginStatus() {
@@ -168,8 +200,6 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onResume() {
         super.onResume();
-
-        checkLoginStatus();
     }
 
     @Override
@@ -196,15 +226,17 @@ public class MainActivity extends AppCompatActivity
     public void onActivityReenter(int resultCode, Intent data) {
         super.onActivityReenter(resultCode, data);
 
-        postponeEnterTransition();
-        //Toast.makeText(this, "postponed transitions", Toast.LENGTH_SHORT).show();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            postponeEnterTransition();
+        }
 
         mReenterState = new Bundle(data.getExtras());
 
         int initialPosition = mReenterState.getInt(CardViewActivity.INITIAL_CARD_POSITION);
         int currentPosition = mReenterState.getInt(CardViewActivity.CURRENT_CARD_POSITION);
 
-        mUpdateRecyclerViewCallback.onUpdateRecyclerViewCallback(initialPosition, currentPosition);
+        EventBus.getDefault().post(
+                new UpdateRecyclerViewPositionReturnEvent(initialPosition, currentPosition));
     }
 
     @Override
@@ -256,14 +288,55 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onCallback(int position) {
         mFragment = null;
+
+        Fragment leftFragment = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+
         switch (position) {
             case 0:
+                if (leftFragment.getClass().equals(SearchFragment.class)) {
+                    break;
+                }
+
                 mFragment = new SearchFragment();
-                mUpdateRecyclerViewCallback = (SearchFragment) mFragment;
+
+                if (getResources().getBoolean(R.bool.multipane)) {
+                    /*getSupportFragmentManager().beginTransaction()
+                            .remove(rightFragment)
+                            .commit();*/
+
+                    // TODO: Pull this from the sharedpreferences instead
+                    CardSearchParameters cardSearchParameters = new CardSearchParameters();
+
+                    SetArrayList setFilter = new SetArrayList();
+                    SetItem setItem = new SetItem();
+                    setItem.setName("Oath of the Gatewatch");
+                    setItem.setCode("OGW");
+                    setFilter.add(setItem);
+
+                    cardSearchParameters.setSetFilter(setFilter);
+
+                    CardViewFragment cardViewFragment = CardViewFragment.newInstance(0, 0, cardSearchParameters);
+
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_container_right, cardViewFragment, "CardViewFragment")
+                            .commit();
+                }
 
                 break;
             case 3:
+                if (leftFragment.getClass().equals(FavoritesFragment.class)) {
+                    break;
+                }
+
                 mFragment = new FavoritesFragment();
+
+                if (getResources().getBoolean(R.bool.multipane)) {
+                    CardViewFragment cardViewFragment = CardViewFragment.newInstance(0, 0, null);
+
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_container_right, cardViewFragment, "CardViewFragment")
+                            .commit();
+                }
 
                 break;
             default:
@@ -314,6 +387,12 @@ public class MainActivity extends AppCompatActivity
             editor.putString("PREF_LOGGED_IN_NAME", account.getDisplayName());
             editor.putString("PREF_LOGGED_IN_EMAIL", account.getEmail());
 
+            if (account.getPhotoUrl() != null) {
+                editor.putString("PREF_PROFILE_IMAGE_URL", account.getPhotoUrl().toString());
+            } else {
+                editor.putString("PREF_PROFILE_IMAGE_URL", "");
+            }
+
             if (mSigningInOut) {
                 Snackbar.make(findViewById(R.id.main_coord),
                         "Successfully Logged In", Snackbar.LENGTH_SHORT).show();
@@ -326,6 +405,7 @@ public class MainActivity extends AppCompatActivity
             editor.putString("PREF_LOGGED_IN_ID", "");
             editor.putString("PREF_LOGGED_IN_NAME", "");
             editor.putString("PREF_LOGGED_IN_EMAIL", "");
+            editor.putString("PREF_PROFILE_IMAGE_URL", "");
 
             if (mSigningInOut) {
                 Snackbar.make(findViewById(R.id.main_coord),
@@ -367,4 +447,51 @@ public class MainActivity extends AppCompatActivity
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
         Log.d(TAG, "onConnectionFailed:" + connectionResult);
     }
+
+    @Override
+    public void onLoadCardDetailCallback(int position, long selectedItemId, View transitionView,
+                                         CardSearchParameters searchParameters) {
+        Intent viewCardIntent = new Intent(this, CardViewActivity.class);
+
+        if (getResources().getBoolean(R.bool.multipane)) {
+            if (getSupportFragmentManager().findFragmentByTag("card_view") == null) {
+                CardViewFragment cardViewFragment = CardViewFragment.newInstance(
+                        position, selectedItemId, searchParameters);
+
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_container_right, cardViewFragment, "card_view")
+                        .commit();
+            } else {
+                CardViewFragment cardViewFragment = (CardViewFragment) getSupportFragmentManager()
+                        .findFragmentByTag("card_view");
+
+                if (cardViewFragment != null) {
+                    cardViewFragment.scrollToPosition(position);
+                }
+            }
+        } else {
+            Bundle bundle = null;
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                bundle = ActivityOptionsCompat
+                        .makeSceneTransitionAnimation(
+                                this,
+                                transitionView,
+                                transitionView.getTransitionName())
+                        .toBundle();
+
+                Log.d("MtMT", transitionView.getTransitionName() + " -> " +
+                        transitionView.getTransitionName());
+            }
+
+            viewCardIntent.putExtra(CardViewActivity.INITIAL_CARD_POSITION, position);
+            viewCardIntent.putExtra(CardViewActivity.SELECTED_ITEM_ID,
+                    selectedItemId);
+            viewCardIntent.putExtra(CardViewActivity.SEARCH_PARAMETERS, searchParameters);
+
+            startActivity(viewCardIntent, bundle);
+        }
+    }
+
 }
