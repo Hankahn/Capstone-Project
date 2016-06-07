19b6764be70b5b9bb670b6d7bc2d1c6552f50388
Hankahn <hankahn@gmail.com>
2016-06-07 03:03:14 -0400

    Added ability to click favorites widget to jump to Favorites screen Tried add card images to favorite widget but was unsuccessful

--- MagicTheManaging/app/src/main/java/com/essentialtcg/magicthemanaging/factories/FavoritesViewsFactory.java
+++ MagicTheManaging/app/src/main/java/com/essentialtcg/magicthemanaging/factories/FavoritesViewsFactory.java
@@ -1,9 +1,12 @@
 package com.essentialtcg.magicthemanaging.factories;
 
 import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Binder;
@@ -15,9 +18,14 @@ import android.widget.AdapterView;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.AppWidgetTarget;
 import com.essentialtcg.magicthemanaging.R;
 import com.essentialtcg.magicthemanaging.data.contracts.CardContract;
 import com.essentialtcg.magicthemanaging.data.loaders.CardLoader;
+import com.essentialtcg.magicthemanaging.ui.widgets.FavoritesWidget;
 import com.essentialtcg.magicthemanaging.utils.CardUtil;
 import com.essentialtcg.magicthemanaging.utils.Util;
 
@@ -27,11 +35,12 @@ import com.essentialtcg.magicthemanaging.utils.Util;
 public class FavoritesViewsFactory implements RemoteViewsService.RemoteViewsFactory,
         Loader.OnLoadCompleteListener<Cursor> {
 
-    private static final String TAG = "FavoritesViewsFactory";
+    private static final String TAG = FavoritesViewsFactory.class.getSimpleName();
 
     private Cursor mCursor = null;
     private Context mContext = null;
-    private CardLoader mCardLoader = null;
+
+    private AppWidgetTarget mAppWidgetTarget;
 
     private final String[] FAVORITE_COLUMNS = {
             CardContract.Cards._ID,
@@ -103,7 +112,7 @@ public class FavoritesViewsFactory implements RemoteViewsService.RemoteViewsFact
         if (mCursor.getString(NAME2) != null) {
             views.setTextViewText(R.id.favorites_widget_name_text_view,
                     String.format(
-                            mContext.getResources().getString(R.string.CARD_NAME_DOUBLE_SIDED),
+                            mContext.getResources().getString(R.string.card_name_double_sided),
                             mCursor.getString(NAME),
                             mCursor.getString(NAME2)
                     ));
@@ -112,7 +121,34 @@ public class FavoritesViewsFactory implements RemoteViewsService.RemoteViewsFact
         }
 
         try {
+            mAppWidgetTarget = new AppWidgetTarget(mContext, views, R.id.cropped_image_view, 30, 43,
+                    new ComponentName(mContext.getPackageName(), FavoritesWidget.class.getName())) {
+
+                @Override
+                public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
+                    super.onResourceReady(resource, glideAnimation);
+                }
+
+            };
+
+            final String imageUrl = CardUtil.buildImageUrl(
+                    mContext,
+                    mCursor.getString(CardLoader.Query.IMAGE_NAME),
+                    mCursor.getString(CardLoader.Query.SET_CODE),
+                    Util.dpToPx(mContext, 43));
+
+            Glide
+                    .with(mContext)
+                    .load(imageUrl)
+                    .asBitmap()
+                    .diskCacheStrategy(DiskCacheStrategy.RESULT)
+                    .dontTransform()
+                    .dontAnimate()
+                    .placeholder(R.mipmap.card_back)
+                    .into(mAppWidgetTarget);
+
             int resourceId = CardUtil.parseSetRarity(
+                    mContext,
                     mCursor.getString(SET_CODE),
                     mCursor.getString(RARITY));
 
@@ -129,22 +165,30 @@ public class FavoritesViewsFactory implements RemoteViewsService.RemoteViewsFact
                         resourceId);
                 views.setViewVisibility(R.id.favorites_widget_set_rarity_text_view, View.INVISIBLE);
             } catch (Exception ex) {
-                Log.e("MtM", ex.toString());
+                Log.e(TAG, ex.toString());
                 views.setTextViewText(R.id.favorites_widget_set_rarity_text_view,
-                        String.format("%s %s",
+                        String.format(mContext.getString(R.string.rarity_format),
                                 mCursor.getString(SET_CODE),
                                 mCursor.getString(RARITY).substring(0, 1)));
                 views.setViewVisibility(R.id.favorites_widget_set_rarity_image_view, View.INVISIBLE);
             }
         } catch (Exception ex) {
-            Log.d("MtM", mCursor.getString(SET_CODE)
+            Log.d(TAG, mCursor.getString(SET_CODE)
                     + "_" + mCursor.getString(RARITY));
         }
 
         views.setTextViewText(R.id.favorites_widget_type_text_view,
                 mCursor.getString(TYPE));
 
+        pushWidgetUpdate(mContext, views);
+
         return views;
+    }
+
+    public static void pushWidgetUpdate(Context context, RemoteViews remoteViews) {
+        ComponentName myWidget = new ComponentName(context, AppWidgetProvider.class);
+        AppWidgetManager manager = AppWidgetManager.getInstance(context);
+        manager.updateAppWidget(myWidget, remoteViews);
     }
 
     @Override
